7438ca8210b7a985e4367db78ebbee4ecf346d48
Haris-Siddiqui <shuja0094@gmail.com>
2025-04-14 18:00:23 +0500

    change url

--- src/api/config.js
+++ src/api/config.js
@@ -5,8 +5,8 @@ export const BASE_URL =
 export const WS_URL =
   process.env.REACT_APP_NODE_ENV === "local"
     ? "http://localhost:5000"
-    : "https://apiv3.ams.consoledot.com/api/v1/hr";
+    : "https://apiv3.ams.consoledot.com";
     export const Login_URL =
   process.env.REACT_APP_NODE_ENV === "local"
     ? "http://localhost:5000/api/v1"
-    : "https://apiv3.ams.consoledot.com/api/v1/hr";
+    : "https://apiv3.ams.consoledot.com/api/v1";
